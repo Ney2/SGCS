96d6c54c742b6a697accc09c8e942c336de2e0ef
Doni Abeje <abeje.doni@gmail.com>
2023-04-08 08:48:39 +0300

    draw shortest path

--- app/src/main/java/com/example/sgcs/MapsActivity.java
+++ app/src/main/java/com/example/sgcs/MapsActivity.java
@@ -1,37 +1,41 @@
 package com.example.sgcs;
 
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
-import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.android.PolyUtil;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener, GoogleMap.OnMarkerClickListener {
 
@@ -55,6 +59,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        // This is how you can draw the shortest path between two points
+        LatLng origin = new LatLng(8.960662740046882, 38.77280735505152);
+        LatLng destination = new LatLng(8.976831653490418, 38.74334306685664);
+        drawShortestPath(origin, destination);
 
         StringRequest request = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
@@ -107,6 +116,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         requestQueue.add(request);
 
     }
+
+    private void drawShortestPath(LatLng origin, LatLng destination) {
+        try {
+            String o = origin.latitude + ", " + origin.longitude;
+            String d = destination.latitude + ", " + destination.longitude;
+            // Get shortest path from api
+            DirectionsResult results = getDirectionsResult(o, d);
+            List<LatLng> decodedPath = PolyUtil.decode(results.routes[0].overviewPolyline.getEncodedPath());
+            // Draw the path with polyline
+            mMap.addPolyline(new PolylineOptions().addAll(decodedPath)).setColor(Color.BLUE);
+            // Zoom camera to origin
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(origin));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(origin, 14));
+            // Set markers
+            mMap.addMarker(new MarkerOptions()
+                    .position(origin)
+                    .title("Origin")
+            );
+            mMap.addMarker(new MarkerOptions()
+                    .position(destination)
+                    .title("Destination")
+            );
+
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private DirectionsResult getDirectionsResult(String origin, String destination) throws ApiException, InterruptedException, IOException {
+        return DirectionsApi.newRequest(getGeoContext())
+                .mode(TravelMode.DRIVING)
+                .origin(origin)
+                .destination(destination).await();
+    }
+
+    private GeoApiContext getGeoContext() {
+        String apiKey = "AIzaSyAVZ0xNw49Xslmr9xqB2Md18DEYY7rZiKs";
+        return new GeoApiContext.Builder()
+                .apiKey(apiKey)
+                .build();
+    }
+
 
 
     @Override
